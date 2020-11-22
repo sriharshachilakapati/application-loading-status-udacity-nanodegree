8603c903c7101bdc101cbacade69fd6e708a320a
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2020-11-22 18:07:52 +0530

    Updated dependencies to latest versions

--- solution/app/src/main/java/com/udacity/DetailActivity.kt
+++ solution/app/src/main/java/com/udacity/DetailActivity.kt
@@ -2,14 +2,17 @@ package com.udacity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_detail.*
+import com.udacity.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-        setSupportActionBar(toolbar)
+
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
     }
 
 }
--- solution/app/src/main/java/com/udacity/MainActivity.kt
+++ solution/app/src/main/java/com/udacity/MainActivity.kt
@@ -11,8 +11,7 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import com.udacity.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,14 +22,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
-        custom_button.setOnClickListener {
+        binding.contentMain.customButton.setOnClickListener {
             download()
         }
     }
