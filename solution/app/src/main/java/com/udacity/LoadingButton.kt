25111547db18e190cc5abd0f0fd4fdf41e287985
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2020-11-29 17:37:11 +0530

    Drawing the custom button and text

--- solution/app/src/main/java/com/udacity/LoadingButton.kt
+++ solution/app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,46 +1,57 @@
 package com.udacity
 
-import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
-import kotlin.properties.Delegates
+import java.util.*
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-    private var widthSize = 0
-    private var heightSize = 0
 
-    private val valueAnimator = ValueAnimator()
+    private var buttonState: ButtonState = ButtonState.Completed
+    private val textRect = Rect()
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
-
+    private val backgroundPaint = Paint().apply {
+        isAntiAlias = true
+        color = context.getColor(R.color.colorPrimary)
     }
 
-
-    init {
-
+    private val textPaint = Paint().apply {
+        isAntiAlias = true
+        color = context.getColor(R.color.white)
+        textSize = 16.sp
+        textAlign = Paint.Align.CENTER
+        typeface = Typeface.DEFAULT_BOLD
     }
 
+    private val buttonText: String
+        get() = when (buttonState) {
+            is ButtonState.Completed,
+            is ButtonState.Clicked -> context.resources.getString(R.string.download)
+            is ButtonState.Loading -> context.resources.getString(R.string.button_loading)
+        }.toUpperCase(Locale.ROOT)
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        canvas.drawRoundRect(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat(), 10.dp, 10.dp, backgroundPaint)
+        textPaint.getTextBounds(buttonText, 0, buttonText.length, textRect)
 
+        val centerX = measuredWidth.toFloat() / 2
+        val centerY = measuredHeight.toFloat() / 2 - textRect.centerY()
+
+        canvas.drawText(buttonText, centerX, centerY, textPaint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
-        val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
-        val h: Int = resolveSizeAndState(
-            MeasureSpec.getSize(w),
-            heightMeasureSpec,
-            0
-        )
-        widthSize = w
-        heightSize = h
+        val minW: Int = paddingLeft + paddingRight + suggestedMinimumWidth
+        val w: Int = resolveSizeAndState(minW, widthMeasureSpec, 1)
+        val h: Int = resolveSizeAndState(MeasureSpec.getSize(w), heightMeasureSpec, 0)
+
         setMeasuredDimension(w, h)
     }
-
 }
--- solution/app/src/main/java/com/udacity/UIUtils.kt
+++ solution/app/src/main/java/com/udacity/UIUtils.kt
@@ -7,3 +7,4 @@ fun Number.toUnit(unit: Int): Float =
         TypedValue.applyDimension(unit, toFloat(), Resources.getSystem().displayMetrics)
 
 inline val Number.dp get() = toUnit(TypedValue.COMPLEX_UNIT_DIP)
+inline val Number.sp get() = toUnit(TypedValue.COMPLEX_UNIT_SP)
