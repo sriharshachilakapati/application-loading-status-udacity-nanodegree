21f7d1aed3668529bd0fd99cc0ea803c2832bd31
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2020-12-01 02:35:44 +0530

    Show notification after download finishes with action to DetailActivity

--- solution/app/src/main/java/com/goharsha/loadapp/MainActivity.kt
+++ solution/app/src/main/java/com/goharsha/loadapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.goharsha.loadapp
 
 import android.app.DownloadManager
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -12,23 +11,19 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatRadioButton
-import androidx.core.app.NotificationCompat
+import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import com.goharsha.loadapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var downloadID: Long = 0
-
-    private lateinit var notificationManager: NotificationManager
-    private lateinit var pendingIntent: PendingIntent
-    private lateinit var action: NotificationCompat.Action
-
     private lateinit var binding: ActivityMainBinding
 
+    private var downloadID: Long = 0
     private val downloadOptions = mutableMapOf<Int, String>()
 
     private val urlToDownload: String?
@@ -85,25 +80,74 @@ class MainActivity : AppCompatActivity() {
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if (downloadID != intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)) {
+                return
+            }
+
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            val query = DownloadManager.Query().setFilterById(downloadID)
+            val cursor = downloadManager.query(query)
+
+            if (cursor.moveToFirst()) {
+                val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                val title = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_TITLE))
+
+                val notificationMessage = if (status == DownloadManager.STATUS_FAILED) {
+                    getString(R.string.download_failed, title)
+                } else {
+                    getString(R.string.download_success, title)
+                }
+
+                val notifyIntent = Intent(this@MainActivity, DetailActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    putExtras(
+                        bundleOf(
+                            "status" to status,
+                            "title" to title,
+                            "notificationId" to downloadID.toInt()
+                        )
+                    )
+                }
+
+                val pendingIntent = PendingIntent.getActivity(
+                    context,
+                    0,
+                    notifyIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                showNotification(
+                    R.id.channel_download_completed,
+                    downloadID.toInt(),
+                    getString(R.string.download_notification_title),
+                    notificationMessage,
+                    getString(R.string.action_show_details),
+                    pendingIntent
+                )
+            }
+
             binding.contentMain.customButton.buttonState = ButtonState.DOWNLOAD
         }
     }
 
     private fun download() {
         if (urlToDownload == null) {
-            Toast.makeText(this, getString(R.string.message_no_option_selected), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.message_no_option_selected), Toast.LENGTH_SHORT)
+                .show()
             return
         }
 
         binding.contentMain.customButton.buttonState = ButtonState.DOWNLOADING
 
+        val selectedOption =
+            findViewById<RadioButton>(binding.contentMain.radioGroup.checkedRadioButtonId)
+
         val request = DownloadManager.Request(Uri.parse(urlToDownload))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
+            .setTitle(selectedOption.text)
+            .setDescription(getString(R.string.app_description))
+            .setRequiresCharging(false)
+            .setAllowedOverMetered(true)
+            .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID = downloadManager.enqueue(request)
--- solution/app/src/main/java/com/goharsha/loadapp/NotificationUtils.kt
+++ solution/app/src/main/java/com/goharsha/loadapp/NotificationUtils.kt
@@ -1,15 +1,18 @@
 package com.goharsha.loadapp
 
-import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.graphics.Color
 import android.os.Build
+import androidx.annotation.IdRes
 import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 
 @RequiresApi(Build.VERSION_CODES.O)
 internal fun LoadApp.createNotificationChannels() {
-    val notificationManager = getSystemService(Application.NOTIFICATION_SERVICE) as NotificationManager
+    val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
     val channelId = resources.getResourceName(R.id.channel_download_completed)
     val channelName = getString(R.string.channel_download_completed_name)
@@ -21,5 +24,29 @@ internal fun LoadApp.createNotificationChannels() {
         enableVibration(true)
 
         notificationManager.createNotificationChannel(this)
+    }
+}
+
+fun Context.showNotification(
+    @IdRes channelId: Int,
+    notificationId: Int,
+    title: String,
+    message: String,
+    actionTitle: String? = null,
+    onActionClick: PendingIntent? = null
+) {
+    with(NotificationCompat.Builder(this, resources.getResourceName(channelId))) {
+        setContentTitle(title)
+        setContentText(message)
+        setSmallIcon(R.drawable.ic_assistant_black_24dp)
+        setAutoCancel(true)
+
+        actionTitle?.let {
+            addAction(NotificationCompat.Action(null, actionTitle, onActionClick))
+        }
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(notificationId, build())
     }
 }
